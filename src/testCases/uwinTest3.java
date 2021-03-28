91393af01a0de5c7809ac590419947e531d94b45
Anthony Tortola <57362052+AJTortola@users.noreply.github.com>
2021-03-28 13:48:03 -0400

    Update and rename uwinTest6.java to uwinTest3.java

--- src/testCases/uwinTest3.java
+++ src/testCases/uwinTest3.java
@@ -1,4 +1,5 @@
 package testCases;
+//Description: This code will go to uwindsor.ca and open all of the social media links located in the footer of the page in new tabs.  It will verify the expected number of links with the actual number of links.
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -9,14 +10,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class uwinTest6 {
 
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ajtor\\Downloads\\chromedriver_win32\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.get("http://www.uwindsor.ca/");
-		driver.manage().window().maximize();
-		WebElement opentabs=driver.findElement(By.xpath("//div[@class='container']"));
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ajtor\\Downloads\\chromedriver_win32\\chromedriver.exe"); //Path where chrome web driver is located
+		WebDriver driver = new ChromeDriver(); //Creating a new web driver
+		driver.get("http://www.uwindsor.ca/"); //Tells driver to go to uwindsor.ca
+		driver.manage().window().maximize(); //Maximizes the window
+		WebElement opentabs=driver.findElement(By.xpath("//div[@class='container']")); //Create WebElement opentabs which equals the xpath for the Social Media Links. 
 		
 		
-		for(int i=0;i<opentabs.findElements(By.tagName("a")).size();i++)
+		for(int i=0;i<opentabs.findElements(By.tagName("a")).size();i++) //This for loop will open all of the links in the footer in new tabs
 		{
 			String opentabsagain=Keys.chord(Keys.CONTROL,Keys.ENTER);
 			opentabs.findElements(By.tagName("a")).get(i).sendKeys(opentabsagain);
