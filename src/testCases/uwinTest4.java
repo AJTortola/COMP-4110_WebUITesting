8955f15843267bc1bd3e2332b56d9b08cf84c3be
Anthony Tortola <57362052+AJTortola@users.noreply.github.com>
2021-03-28 13:59:56 -0400

    Update uwinTest4.java

--- src/testCases/uwinTest4.java
+++ src/testCases/uwinTest4.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class uwinTest1 {
+public class uwinTest4 {
 
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ajtor\\Downloads\\chromedriver_win32\\chromedriver.exe"); //Path where chrome web driver is located
